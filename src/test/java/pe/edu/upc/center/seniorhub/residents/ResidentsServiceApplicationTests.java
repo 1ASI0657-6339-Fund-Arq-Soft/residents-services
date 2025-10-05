05132ac85bc70ae429596f2a76e3cf0abd7ab75c
Andres-0304 <andresfer0304@gmail.com>
2025-10-05 15:22:09 -0500

    chore: initial commit

---
+++ src/test/java/pe/edu/upc/center/seniorhub/residents/ResidentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.center.seniorhub.residents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResidentsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
